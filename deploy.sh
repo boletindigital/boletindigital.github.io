95381afb3bf520e0128e330f24f9ca16e514b129
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2016-09-11 17:56:08 -0500

    Corrección a script deploy.sh de travis-ci

--- deploy.sh
+++ deploy.sh
@@ -54,7 +54,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in travis@boletin.digital.enc -out travis@boletin.digital -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../travis@boletin.digital.enc -out travis@boletin.digital -d
 chmod 600 travis@boletin.digital
 eval `ssh-agent -s`
 ssh-add travis@boletin.digital
