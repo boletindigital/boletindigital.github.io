ceb3c67e4b6faadb18bad10bd4e96e128e4ae846
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2016-09-11 17:40:00 -0500

    Configuración, entorno y script para hacer deploy automágicamente con travis-ci

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ SOURCE_BRANCH="source"
 TARGET_BRANCH="master"
 
 function doCompile {
-  ./compile.sh
+  gulp build
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
@@ -20,21 +20,21 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Clone the existing gh-pages for this repo into out/
+# Clone the existing gh-pages for this repo into dist/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
-cd out
+git clone $REPO dist
+cd dist
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clean out existing contents
-rm -rf out/**/* || exit 0
+rm -rf dist/**/* || exit 0
 
 # Run our compile script
-gulp
+doCompile
 
 # Now let's go have some fun with the cloned repo
-cd out
+cd dist
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
@@ -54,10 +54,10 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in travis@boletin.digital.enc -out travis@boletin.digital -d
+chmod 600 travis@boletin.digital
 eval `ssh-agent -s`
-ssh-add deploy_key
+ssh-add travis@boletin.digital
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
